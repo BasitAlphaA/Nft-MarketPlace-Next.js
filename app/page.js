6ae6070aba6c8ad6c71256e97bf698def6fa6e70
Abdul Basit <itxmianabdulbasit@gmail.com>
2024-12-26 20:22:21 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,16 +4,36 @@ import NFTBox from "../components/NFTBox";
 import networkMapping from "../constants/networkMapping.json";
 import GET_ACTIVE_ITEMS from "../constants/subgraphQueries";
 import { useQuery } from "@apollo/client";
+import { useState, useEffect } from "react";
 
 
 
 
 export default function Home() {
   const { chainId, isWeb3Enabled } = useMoralis();
-  const chainString = chainId ? parseInt(chainId).toString() : null;
-  const marketplaceAddress = chainId ? networkMapping[chainString].NftMarketplace[0] : null;
+  const [marketplaceAddress, setMarketplaceAddress] = useState(null);
 
-  const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS);
+  // const chainString = chainId ? parseInt(chainId).toString() : null;
+
+  useEffect(() => {
+    if (chainId) {
+      const chainString = parseInt(chainId).toString();
+      const address = networkMapping[chainString]?.NftMarketplace?.[0];
+      setMarketplaceAddress(address);
+    }
+  }, [chainId]);
+
+  // Call `useQuery` unconditionally
+  const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS, {
+    skip: !isWeb3Enabled || !marketplaceAddress, // Skip the query if conditions are not met
+  });
+
+  // Early return if web3 is not enabled or marketplace address is not set
+  if (!isWeb3Enabled || !marketplaceAddress) {
+    return <div>Loading...</div>; // Show a loading message or spinner
+  }
+
+  console.log("marketplace address", marketplaceAddress);
 
   return (
          <div className="container mx-auto ">
@@ -47,6 +67,13 @@ export default function Home() {
 
   );
 }
+
+
+// 1. updateModal ***DONE***
+// 2. NFTbox  ***DONE***
+// 3. page.js ***DONE***
+// 4. sell-nft ***DONE***
+
 
 
 // 1. updateModal ***DONE***
